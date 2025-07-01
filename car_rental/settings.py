1ac4ea0c0a328e50d7458ccd1b8000d907c0c4b1
Haymanot Asmare <maniga.1love@gmail.com>
2025-07-02 08:15:56 +1100

    `Update static file settings and paths`

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -169,12 +169,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifestStaticFilesStorage'
-MIDDLEWARE.insert(1,'whitenoise.middleware.whiteNoiseMiddleware')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
